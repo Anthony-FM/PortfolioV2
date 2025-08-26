efc92397af393fa297ae1baef9bf0482293bcbf7
Anthony Fouda Many <anthony.foudamany@gmail.com>
2025-08-26 11:34:27 +0200

    :construction: test

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import ButtonAnimated from '../../components/Button/ButtonAnimated'
-import FlipDigit from '../../components/FilpLoader/FilpDigit.jsx/FlipDigit'
-import FlipCounter from '../../components/FilpLoader/FlipCounter'
+import { motion, useScroll, useTransform } from "framer-motion";
 
-function Test() {
+export default function Test() {
+  // const { scrollY } = window.document.;
+  // const scale = useTransform(scrollY, [0, 500], [500, 3]); // adapte selon ta page
+
+  window.addEventListener("scroll", () => {
+  console.log(window.scrollY); // valeur en pixels du scroll vertical
+});
+
+
   return (
-    <div>
-        <FlipCounter/>
+  
+  <>
+    <div className="container">
+      <div className="hero">
+        <img 
+          src="src/assets/pictureOfMeBW.jpg" 
+          alt="" 
+          className="hero-image" 
+          style={{
+            WebkitMaskSize: "100%"
+          }}
+        />        
+      </div>
     </div>
-  )
-}
+    <section
+      style={{
+        width: "100%",
+        height: "2000px"
+      }}
+    >
 
-export default Test
+    </section>
+    
+  </>
+  );
+}
